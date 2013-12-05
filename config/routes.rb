a89f8f359e230dc2e19a25b955c072cda87925c0
Rikke Willer <riwi@dtic.dtu.dk>
2013-12-05 09:19:54 +0100

    Update rails version, refs #1376

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Keikoku::Application.routes.draw do
   
-  get '/alerts/find/'
+  get '/alerts/find/', to: 'alerts#find'
   resources :alerts, except: [:new, :edit] do
     resources :alert_stats, only: [:index]
   end
